4dfc6c545deeab97c0c723e6887f28dcf6b14c8f
guerler <aysam.guerler@gmail.com>
2025-05-04 01:50:36 -0400

    Add prebuild jupyter and index.js

---
+++ packages/jupyter/index.js
@@ -0,0 +1,22 @@
+// Access container element
+const appElement = document.querySelector("#app");
+
+// Access attached data
+const incoming = JSON.parse(appElement?.dataset.incoming || "{}");
+const datasetId = incoming.visualization_config.dataset_id;
+const datasetUrl = incoming.visualization_config.dataset_url;
+const root = incoming.root;
+
+// Build query string
+const query = new URLSearchParams({
+    dataset_id: datasetId,
+    dataset_url: datasetUrl || `${root}api/datasets/${datasetId}/display`,
+    root,
+}).toString();
+
+// Load jupyter
+(function () {
+    const jupyterRoot = `${root}static/plugins/visualizations/jupyter/static/dist/_output`;
+    const jupyterTarget = `${jupyterRoot}/lab/index.html?${query}`;
+    window.location.replace(jupyterTarget);
+})();
