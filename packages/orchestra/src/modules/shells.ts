3f7aba83acf0e1f44552985a7066c83962cd56cd
guerler <aysam.guerler@gmail.com>
2026-01-02 15:48:24 +0300

    Add to shell list

--- packages/orchestra/src/modules/shells.ts
+++ packages/orchestra/src/modules/shells.ts
@@ -5,12 +5,20 @@ import { BoxPlotShell } from "@/modules/shells/boxplot";
 import { DensityShell } from "@/modules/shells//density";
 import { ScatterShell } from "@/modules/shells//scatter";
 import { BarCountShell } from "@/modules/shells/bar_count";
+import { BarValueShell } from "./shells/bar_value";
+import { BarAggregateShell } from "./shells/bar_aggregate";
+import { LineTimeShell } from "./shells/line_time";
+import { LineGenericShell } from "./shells/line_generic";
 
 export const shells: Record<string, ShellType> = {
-    barcount: new BarCountShell(),
+    bar_aggregate: new BarAggregateShell(),
+    bar_count: new BarCountShell(),
+    bar_value: new BarValueShell(),
     boxplot: new BoxPlotShell(),
     density: new DensityShell(),
     heatmap_correlation: new HeatmapCorrelationShell(),
     histogram: new HistogramShell(),
+    line_generic: new LineGenericShell(),
+    line_time: new LineTimeShell(),
     scatter: new ScatterShell(),
 };
