927f22274379654ac718d0551facbb1883db5408
guerler <aysam.guerler@gmail.com>
2026-01-06 00:34:27 +0300

    Fix runtime

--- packages/vintent/vintent/vintent/core/runtime.py
+++ packages/vintent/vintent/vintent/core/runtime.py
@@ -4,5 +4,4 @@ from .modules.runner import Runner
 async def run(inputs, config):
     runner = Runner(config)
     reply = await runner.run(inputs["transcripts"])
-    print(reply)
-    return "Welcome to VIntent"
+    return reply
