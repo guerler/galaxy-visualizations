7648a8ffe32153cb71465c041d38d2c2e3277cf7
guerler <aysam.guerler@gmail.com>
2026-07-22 09:17:46 +0300

    Use default vite config

--- packages/jsonviewer/vite.config.charts.js
+++ packages/jsonviewer/vite.config.charts.js
@@ -1,87 +1,58 @@
 import { defineConfig } from "vite";
 
 const env = {
-  GALAXY_DATASET_ID: "",
-  GALAXY_KEY: "",
-  GALAXY_ROOT: "",
+    GALAXY_DATASET_ID: "",
+    GALAXY_KEY: "",
+    GALAXY_ROOT: "http://127.0.0.1:8080",
 };
 
 Object.keys(env).forEach((key) => {
-  if (process.env[key]) {
-    env[key] = process.env[key];
-  } else {
-    console.log(
-      `${key} not available. Please provide as environment variable.`,
-    );
-  }
+    if (process.env[key]) {
+        env[key] = process.env[key];
+    } else {
+        console.log(`${key} not available. Please provide as environment variable.`);
+    }
 });
 
-const galaxyRoot = env.GALAXY_ROOT || "http://127.0.0.1:8080";
-
-function isValidUrl(url) {
-  try {
-    new URL(url);
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-const serverConfig = {};
-if (isValidUrl(galaxyRoot)) {
-  serverConfig.proxy = {
-    "/api": {
-      changeOrigin: true,
-      target: galaxyRoot,
-      rewrite: (path) => {
+const proxyGalaxy = () => ({
+    changeOrigin: true,
+    rewrite: (path) => {
         if (env.GALAXY_KEY) {
-          const separator = path.includes("?") ? "&" : "?";
-          return `${path}${separator}key=${env.GALAXY_KEY}`;
-        } else {
-          return path;
+            const separator = path.includes("?") ? "&" : "?";
+            return `${path}${separator}key=${env.GALAXY_KEY}`;
         }
-      },
+        return path;
     },
-  };
-} else {
-  console.log(
-    `GALAXY_ROOT "${galaxyRoot}" is not a valid URL. Skipping proxy setup.`,
-  );
-}
+    target: env.GALAXY_ROOT,
+});
 
-export default defineConfig({
-  build: {
-    outDir: "./static",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: () => "app.js",
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name][extname]",
-      },
+// https://vitejs.dev/config/
+export const viteConfigCharts = defineConfig({
+    base: "./",
+    build: {
+        outDir: "./static",
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                manualChunks: () => "app.js",
+                entryFileNames: "[name].js",
+                chunkFileNames: "[name].js",
+                assetFileNames: "[name][extname]",
+            },
+        },
     },
-  },
-  define: {
-    "process.env.credentials": JSON.stringify(
-      env.GALAXY_KEY ? "omit" : "include",
-    ),
-    "process.env.dataset_id": JSON.stringify(env.GALAXY_DATASET_ID),
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
+    define: {
+        "process.env.credentials": JSON.stringify(env.GALAXY_KEY ? "omit" : "include"),
+        "process.env.dataset_id": JSON.stringify(env.GALAXY_DATASET_ID),
     },
-  },
-  server: {
-    ...serverConfig,
-    watch: {
-      ignored: ["**/node_modules/**", "**/test-data/**"],
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
     },
-    cors: true,
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+    server: {
+        proxy: {
+            "/api": proxyGalaxy(),
+        },
     },
-  },
 });
